fe0f1d83f7c8039e0f89fdb3843f18dd82ac41d0
Felipe <ftspenha@gmail.com>
2022-04-15 12:40:41 -0300

    third commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Welcome to <a href='https://nextjs.org'>É do Fernandinho!</a>
+					Avante Gesso <a href='https://nextjs.org'>é do Fernandinho!</a>
 				</h1>
 
 				<p className={styles.description}>
